90d92cd73de3c4712bca533701e949024bf5b384
Taciano Lopes Amorim <taciano.amorim@tjpe.jus.br>
2018-04-30 02:17:32 -0300

    adicionado os arquivos dos times e escudos, adicionado tela de prototipo relativo ao processamento das partidas e adicionado o req 06 no documento

--- 02- desenvolvimento/footline/src/main/java/com/g2t/footline/view/AppMain.java
+++ 02- desenvolvimento/footline/src/main/java/com/g2t/footline/view/AppMain.java
@@ -1,10 +1,16 @@
 package com.g2t.footline.view;
 
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
  * @author Taciano
@@ -17,6 +23,8 @@ public class AppMain extends javax.swing.JFrame {
 	private static final long serialVersionUID = 6372065349465603650L;
 
 	private javax.swing.JButton btnNovo;
+	
+	private Image img;
 
 	/**
 	 * Creates new form Principal
@@ -48,22 +56,50 @@ public class AppMain extends javax.swing.JFrame {
 						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 413, GroupLayout.PREFERRED_SIZE)
 						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
 		btnNovo = new javax.swing.JButton();
-		btnNovo.setBounds(214, 67, 153, 29);
+		btnNovo.setBounds(207, 65, 139, 29);
 		btnNovo.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnNovo.setIcon(null);
 
-		btnNovo.setText("Novo jogo 1");
+		btnNovo.setText("Novo jogo");
 		btnNovo.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				btnNovoActionPerformed(evt);
 			}
 		});
 		panel.add(btnNovo);
+		
+		JButton btnJogosSalvos = new JButton();
+		btnJogosSalvos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnJogosSalvos.setText("Jogos salvos");
+		btnJogosSalvos.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnJogosSalvos.setBounds(207, 133, 139, 29);
+		panel.add(btnJogosSalvos);
+		
+		JButton btnSobre = new JButton();
+		btnSobre.setText("Sobre");
+		btnSobre.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnSobre.setBounds(207, 199, 139, 29);
+		panel.add(btnSobre);
+		
+		JButton btnSair = new JButton();
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				syste
+			}
+		});
+		btnSair.setText("Sair");
+		btnSair.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnSair.setBounds(207, 267, 139, 29);
+		panel.add(btnSair);
 		getContentPane().setLayout(groupLayout);
 
 		pack();
 	}
 
+	  
 	private void btnNovoActionPerformed(java.awt.event.ActionEvent evt) {
 
 	}
