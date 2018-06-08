f062a7b39e28f698cb5578bfc07d9c252900f579
Taciano amorim <tacianolamorim@gmail.com>
2018-06-08 01:24:49 -0300

    limpeza de codigo e ajustes na entidades

--- 02-desenvolvimento/footline/src/main/java/com/g2t/footline/entity/Escalacao.java
+++ 02-desenvolvimento/footline/src/main/java/com/g2t/footline/entity/Escalacao.java
@@ -15,119 +15,54 @@ public class Escalacao implements Serializable {
 	private Tatica tatica;
 
 	// Titular
-	private Jogador goleiro;
-	private List<Jogador> listaTitularDefesa = new ArrayList<Jogador>();
-	private List<Jogador> listaTitularMeioCampo = new ArrayList<Jogador>();
-	private List<Jogador> listaTitularAtaque = new ArrayList<Jogador>();
+	private List<Jogador> titulares = new ArrayList<Jogador>();
 
 	// Reservas
-	private List<Jogador> listaReservaGoleiro = new ArrayList<Jogador>();
-	private List<Jogador> listaReservaDefesa = new ArrayList<Jogador>();
-	private List<Jogador> listaReservaMeioCampo = new ArrayList<Jogador>();
-	private List<Jogador> listaReservaAtaque = new ArrayList<Jogador>();
+	private List<Jogador> reservas = new ArrayList<Jogador>();
 
 	// Andamento Partida
 	private List<Jogador> listaCartaoAmarelo = new ArrayList<Jogador>();
 	private List<Jogador> listaCartaoVermelho = new ArrayList<Jogador>();
 	private List<Jogador> listaLesionado = new ArrayList<Jogador>();
-
 	public Clube getClube() {
 		return clube;
 	}
 	public void setClube(Clube clube) {
 		this.clube = clube;
 	}
-
 	public Tatica getTatica() {
 		return tatica;
 	}
 	public void setTatica(Tatica tatica) {
 		this.tatica = tatica;
 	}
-	
-	public Jogador getGoleiro() {
-		return goleiro;
+	public List<Jogador> getTitulares() {
+		return titulares;
 	}
-	public void setGoleiro(Jogador goleiro) {
-		this.goleiro = goleiro;
+	public void setTitulares(List<Jogador> titulares) {
+		this.titulares = titulares;
 	}
-
-	public List<Jogador> getListaTitularDefesa() {
-		return listaTitularDefesa;
+	public List<Jogador> getReservas() {
+		return reservas;
 	}
-
-	public void setListaTitularDefesa(List<Jogador> listaTitularDefesa) {
-		this.listaTitularDefesa = listaTitularDefesa;
+	public void setReservas(List<Jogador> reservas) {
+		this.reservas = reservas;
 	}
-
-	public List<Jogador> getListaTitularMeioCampo() {
-		return listaTitularMeioCampo;
-	}
-
-	public void setListaTitularMeioCampo(List<Jogador> listaTitularMeioCampo) {
-		this.listaTitularMeioCampo = listaTitularMeioCampo;
-	}
-
-	public List<Jogador> getListaTitularAtaque() {
-		return listaTitularAtaque;
-	}
-
-	public void setListaTitularAtaque(List<Jogador> listaTitularAtaque) {
-		this.listaTitularAtaque = listaTitularAtaque;
-	}
-
-	public List<Jogador> getListaReservaGoleiro() {
-		return listaReservaGoleiro;
-	}
-
-	public void setListaReservaGoleiro(List<Jogador> listaReservaGoleiro) {
-		this.listaReservaGoleiro = listaReservaGoleiro;
-	}
-
-	public List<Jogador> getListaReservaDefesa() {
-		return listaReservaDefesa;
-	}
-
-	public void setListaReservaDefesa(List<Jogador> listaReservaDefesa) {
-		this.listaReservaDefesa = listaReservaDefesa;
-	}
-
-	public List<Jogador> getListaReservaMeioCampo() {
-		return listaReservaMeioCampo;
-	}
-
-	public void setListaReservaMeioCampo(List<Jogador> listaReservaMeioCampo) {
-		this.listaReservaMeioCampo = listaReservaMeioCampo;
-	}
-
-	public List<Jogador> getListaReservaAtaque() {
-		return listaReservaAtaque;
-	}
-
-	public void setListaReservaAtaque(List<Jogador> listaReservaAtaque) {
-		this.listaReservaAtaque = listaReservaAtaque;
-	}
-
 	public List<Jogador> getListaCartaoAmarelo() {
 		return listaCartaoAmarelo;
 	}
-
 	public void setListaCartaoAmarelo(List<Jogador> listaCartaoAmarelo) {
 		this.listaCartaoAmarelo = listaCartaoAmarelo;
 	}
-
 	public List<Jogador> getListaCartaoVermelho() {
 		return listaCartaoVermelho;
 	}
-
 	public void setListaCartaoVermelho(List<Jogador> listaCartaoVermelho) {
 		this.listaCartaoVermelho = listaCartaoVermelho;
 	}
-
 	public List<Jogador> getListaLesionado() {
 		return listaLesionado;
 	}
-
 	public void setListaLesionado(List<Jogador> listaLesionado) {
 		this.listaLesionado = listaLesionado;
 	}
