d30cc699145796941adbaec577c23b1db007bdb5
gtabosa <38812331+gtabosa@users.noreply.github.com>
2018-06-30 17:24:54 -0300

    Atualizando metodos de classe Clube
    Atualizando metodos de classe Clube

--- 02-desenvolvimento/footline/src/com/g2t/footline/negocio/entidades/Clube.java
+++ 02-desenvolvimento/footline/src/com/g2t/footline/negocio/entidades/Clube.java
@@ -20,6 +20,63 @@ public class Clube extends Tecnico{
 			super(id, nome, nivel);
 		}
 
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+		public int getNivel() {
+			return nivel;
+		}
+		public void setNivel(int nivel) {
+			this.nivel = nivel;
+		}
+		
+		public String getAcronomo() {
+			return acronomo;
+		}
+		public void setAcronomo(String acronomo) {
+			this.acronomo = acronomo;
+		}
+		
+		
+		public String getNomeEstadio() {
+			return nomeEstadio;
+		}
+		public void setNomeEstadio(String nomeEstadio) {
+			this.nomeEstadio = nomeEstadio;
+		}
+
+		public String getNomeArquivo() {
+			return nomeArquivo;
+		}
+		public void setNomeArquivo(String nomeArquivo) {
+			this.nomeArquivo = nomeArquivo;
+		}
+		
+		public int getCapacidade() {
+			return capacidade;
+		}
+		public void setCapacidade(int capacidade) {
+			this.capacidade = capacidade;
+		}
+		
+		public double getValorCaixa() {
+			return valorCaixa;
+		}
+		public void setValorCaixa(double valorCaixa) {
+			this.valorCaixa = valorCaixa;
+		}
+		
 		@Override
 		public String toString() {
 			return "Tecnico [getId()=" + getId() + ", getNome()=" + getNome() + "]";
