c0386df692f3d3a2514a01cf74f6416830f69cdc
Taciano amorim <tacianolamorim@gmail.com>
2018-07-28 10:31:26 -0300

    implementacao tela de estadios

--- 02-desenvolvimento/footline/src/com/g2t/footline/gui/telasSecundarias/FrmEstadio.java
+++ 02-desenvolvimento/footline/src/com/g2t/footline/gui/telasSecundarias/FrmEstadio.java
@@ -7,30 +7,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 import com.g2t.footline.negocio.Fachada;
-import com.g2t.footline.negocio.entidades.Partida;
-import com.g2t.footline.negocio.entidades.Rodada;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import javax.swing.SwingConstants;
+import com.g2t.footline.negocio.entidades.Estadio;
+import com.g2t.footline.util.Biblioteca;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class FrmEstadio extends JDialog {
 
 	/**
 	 * serialVersionUID
 	 */
-	private static final long serialVersionUID = 5577059619343394507L;
+	private static final long serialVersionUID = -3835487868126017777L;
 	
 	private final JPanel contentPanel = new JPanel();
+	private JLabel lblImgEstadio;
 
 	/**
 	 * Create the frame.
@@ -42,71 +44,64 @@ public class FrmEstadio extends JDialog {
 		getContentPane().setForeground(Color.WHITE);
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setModal(true);
-		setBounds(100, 100, 720, 447);
+		setBounds(100, 100, 616, 436);
 		getContentPane().setLayout(null);
-		contentPanel.setBounds(0, 0, 720, 369);
+		contentPanel.setBounds(0, 0, 720, 385);
 		contentPanel.setBackground(Color.BLACK);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel);
 		contentPanel.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(5, 5, 154, 359);
-		panel.setBackground(Color.WHITE);
-		contentPanel.add(panel);
+		JPanel pnlNomeEstadio = new JPanel();
+		pnlNomeEstadio.setBounds(5, 5, 178, 390);
+		pnlNomeEstadio.setBackground(Color.WHITE);
+		contentPanel.add(pnlNomeEstadio);
 		
 		DefaultListModel<String> modelo = new DefaultListModel<String>();
-		JList<String> listJogos =  new JList<String>(modelo);
-		listJogos.setVisibleRowCount(10);
-		
-		JScrollPane scrollPane = new JScrollPane(listJogos);
-		
-		// Carregar a lista
-		List<Rodada> rodadas= Fachada.getInstance().listarRodadas();
-		for (Rodada rodada : rodadas) {
-			String textoRodada= "";
-			if ( rodada.getNumero() < 10 )
-				textoRodada= " RODADA: 0"+ rodada.getNumero() +" - "+
-						rodada.getDescricao();
-			else
-				textoRodada= " RODADA: "+ rodada.getNumero() +" - "+
-						rodada.getDescricao();
-			
-			modelo.addElement( textoRodada );
-			
-			for ( Partida partida : rodada.getPartidas() ) {
-				String textoPartida= "   GRUPO " + partida.getGrupo() + " ";
-				
-				if ( partida.getMandante() != null ) { 
-					textoPartida= textoPartida + "   - " + partida.getMandante().getSelecao().getNome();
-					if ( partida.getPublico() == 0 ) {
-						textoPartida= textoPartida + "  X  " + partida.getVisitante().getSelecao().getNome();
-						
-					} else {
-						textoPartida= textoPartida + " "+ partida.getGolsMandante() +" X "+ 	
-									partida.getGolsVisitante() +" "+ partida.getVisitante().getSelecao().getNome();
-					}
-					modelo.addElement( textoPartida );
+		JList<String> listEstadios =  new JList<String>(modelo);
+		listEstadios.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				String estadioSelecionado= listEstadios.getSelectedValue();
+
+				// trata nome para recuperar arquivo
+				if ( estadioSelecionado != null && estadioSelecionado.length() > 0 ) {
+					estadioSelecionado= Biblioteca.removerAcentosEspacos( estadioSelecionado );
+					lblImgEstadio.setIcon(new ImageIcon(
+							FrmEstadio.class.getResource("/imagens/estadios/"+estadioSelecionado+".png")));
 				}
 			}
+		});
+		listEstadios.setVisibleRowCount(10);
+		
+		JScrollPane scrollPane = new JScrollPane(listEstadios);
+		
+		// Carregar a lista
+		List<Estadio> estadios= Fachada.getInstance().listarEstadio();
+		String nomeEstadio= null;
+		for (Estadio estadio : estadios) {
+			modelo.addElement( estadio.getNome() );
+			// Pega o primeiro nome para exibir no painel de imagem
+			if (nomeEstadio == null) {
+				nomeEstadio= Biblioteca.removerAcentosEspacos( estadio.getNome() );
+				listEstadios.setSelectedIndex(0);
+			}
 		}
-		panel.setLayout(new BorderLayout(0, 0));
+		pnlNomeEstadio.setLayout(new BorderLayout(0, 0));
+		pnlNomeEstadio.add(scrollPane);
 		
-		panel.add(scrollPane);
+		JPanel pnlFoto = new JPanel();
+		pnlFoto.setBackground(Color.WHITE);
+		pnlFoto.setBounds(193, 5, 410, 390);
+		contentPanel.add(pnlFoto);
+		pnlFoto.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.BLUE);
-		panel_1.setBounds(169, 5, 539, 353);
-		contentPanel.add(panel_1);
-		panel_1.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon(FrmEstadio.class.getResource("/imagens/estadios/estadioecaterimburgo.png")));
-		lblNewLabel.setBounds(10, 0, 529, 394);
-		panel_1.add(lblNewLabel);
+		lblImgEstadio = new JLabel("");
+		lblImgEstadio.setIcon(new ImageIcon(FrmEstadio.class.getResource("/imagens/estadios/"+nomeEstadio+".png")));
+		pnlFoto.add(lblImgEstadio);
 		{
 			JPanel buttonPane = new JPanel();
-			buttonPane.setBounds(0, 403, 720, 33);
+			buttonPane.setBounds(10, 392, 596, 33);
 			buttonPane.setBackground(Color.BLACK);
 			buttonPane.setForeground(Color.BLACK);
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
